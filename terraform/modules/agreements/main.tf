42c3173c894722f2ee87e9ed2ea081475b956a87
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-06-23 15:31:46 +0100

    Feature/sfat 159 sg review (#8)
    * SFAT-159 - security group review
    
    * SFAT-159 - formatting fix
    
    * SFAT-159 - reset sg egress CIDR restrictions

--- terraform/modules/agreements/main.tf
+++ terraform/modules/agreements/main.tf
@@ -14,21 +14,16 @@ resource "aws_security_group" "allow_postgres_external" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 5432
-    to_port   = 5432
-    protocol  = "tcp"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    # Tried limiting egress to web & app subnets - could not get a local connection via SSH tunneling
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 5432
-    to_port   = 5432
-    protocol  = "tcp"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -46,12 +41,12 @@ resource "aws_db_subnet_group" "agreements" {
 }
 
 data "aws_ssm_parameter" "master_username" {
-  name = "${lower(var.environment)}-agreements-db-master-username"
+  name            = "${lower(var.environment)}-agreements-db-master-username"
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "master_password" {
-  name = "${lower(var.environment)}-agreements-db-master-password"
+  name            = "${lower(var.environment)}-agreements-db-master-password"
   with_decryption = true
 }
 
