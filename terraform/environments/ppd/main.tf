4889ce33d8b77a63f4d361a4814c9f6d5d56cbb0
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-07-14 14:34:19 +0100

    SFAT-192 - added PPD and PRD environments

---
+++ terraform/environments/ppd/main.tf
@@ -0,0 +1,40 @@
+#########################################################
+# Environment: PPD
+#
+# Deploy SCALE shared databases
+#########################################################
+terraform {
+  backend "s3" {
+    bucket         = "scale-terraform-state"
+    key            = "ccs-scale-infra-db-shared-ppd"
+    region         = "eu-west-2"
+    dynamodb_table = "scale_terraform_state_lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+locals {
+  environment        = "PPD"
+  availability_zones = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+}
+
+data "aws_ssm_parameter" "aws_account_id" {
+  name = "account-id-${lower(local.environment)}"
+}
+
+module "deploy" {
+  source                          = "../../modules/configs/deploy-all"
+  aws_account_id                  = data.aws_ssm_parameter.aws_account_id.value
+  environment                     = local.environment
+  availability_zones              = local.availability_zones
+  deletion_protection             = false
+  skip_final_snapshot             = false
+  enabled_cloudwatch_logs_exports = ["postgresql"]
+  backup_retention_period         = 35
+  agreements_cluster_instances    = length(local.availability_zones)
+}
